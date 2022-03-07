0f755c0cf1bfe07485b0dc87dd3bd09b39edb389
BreathlessWay <731005087@qq.com>
2022-03-07 17:59:27 +0800

    update

--- write/currying.js
+++ write/currying.js
@@ -1,15 +1,14 @@
 const currying = (fn, ...args) => {
-	const funLen = fn.length,
+	const fnLen = fn.length,
 		argLen = args.length;
-	if (argLen < funLen) {
-		return (...rest) => {
-			return currying(fn, ...args, ...rest);
-		};
-	} else {
+
+	if (fnLen === argLen) {
 		return fn(...args);
+	} else {
+		return (...rest) => currying(fn, ...args, ...rest);
 	}
 };
 
 const add = (a, b, c) => a + b + c;
 const a = currying(add, 1);
-console.log(a(2,3))
+console.log(a(2, 3));
