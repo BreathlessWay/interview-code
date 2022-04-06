181238e7072cb662b8a055b9f213b1990cf6bc55
BreathlessWay <731005087@qq.com>
2022-04-06 21:49:43 +0800

    no message

--- structures/maxNotRepeat.js
+++ structures/maxNotRepeat.js
@@ -1,27 +1,25 @@
 const maxNotRepeat = (str) => {
-	let len = str.length,
-		maxLen = 0,
+	let maxLen = 0,
 		start = 0,
 		res = "",
 		map = new Map();
 
-	for (let i = 0; i < len; i++) {
-		const item = str[i];
-		if (map.has(item)) {
-			start = Math.max(start, map.get(item) + 1);
+	for (let i = 0; i < str.length; i++) {
+		const v = str[i];
+		if (map.has(v)) {
+			start = Math.max(start, map.get(v) + 1);
 		}
 
-		map.set(item, i);
-		if (i - start + 1 > maxLen) {
+		map.set(v, i);
+		if (maxLen < i - start + 1) {
 			maxLen = i - start + 1;
 			res = str.slice(start, i + 1);
 		}
 	}
-
 	console.log(maxLen, res);
 };
 
 maxNotRepeat("abcabcbb");
 maxNotRepeat("bbbbb");
 maxNotRepeat("pwwkew");
-maxNotRepeat("");
+maxNotRepeat("a");
--- structures/mergeSort.js
+++ structures/mergeSort.js
@@ -2,7 +2,7 @@ const merge = (left, right) => {
 	let res = [], i = 0, j = 0;
 
 	while (i < left.length && j < right.length) {
-		if (left[i] > right[j]) {
+		if (left[i] < right[j]) {
 			res.push(left[i]);
 			i++;
 		} else {
@@ -24,14 +24,14 @@ const mergeSort = (arr) => {
 	const len = arr.length;
 
 	if (len < 2) {
-		return arr;
+		return arr[0];
 	}
 
-	const mid = Math.floor(len / 2);
-
-	const left = mergeSort(arr.slice(0, mid)), right = mergeSort(arr.slice(mid));
+	const mid = Math.floor(len / 2),
+		left = mergeSort(arr.slice(0, mid)),
+		right = mergeSort(arr.slice(mid));
 
 	return merge(left, right);
 };
 
-console.log(mergeSort([9, 5, 8, 1, 7]));
+console.log(mergeSort([[1, 2, 4], [2, 3, 7], [3, 5, 7], [4, 5, 8]]));
