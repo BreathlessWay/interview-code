c1be5d036253e7b3d1f37853c9bdcb133781eed3
BreathlessWay <731005087@qq.com>
2022-05-18 16:00:14 +0800

    并发size

--- write/Scheduler.js
+++ write/Scheduler.js
@@ -19,9 +19,7 @@ class Scheduler {
 	};
 
 	request = () => {
-		const sq = this.splitQueue(),
-			len = sq.length,
-			current = sq[this.count];
+		const sq = this.splitQueue(), len = sq.length, current = sq[this.count];
 
 		return Promise.all(current).then(res => {
 			this.result = this.result.concat(res);
@@ -33,6 +31,30 @@ class Scheduler {
 			}
 		});
 	};
+
+	mm = () => new Promise(resolve => {
+		const len = this.queue.length;
+		let result = [], count = 0;
+
+		const cb = () => {
+			const index = len - this.queue.length, current = this.queue.splice(0, 1)[0];
+			current.then(res => {
+				result[index] = res;
+				count++;
+				if (count === len) {
+					resolve(result);
+					return;
+				}
+				if (this.queue.length) {
+					cb();
+				}
+			});
+		};
+
+		for (let i = 0; i < this.limit; i++) {
+			cb();
+		}
+	});
 }
 
 const requestControl = new Scheduler(4);
@@ -41,4 +63,4 @@ for (let i = 0; i < 20; i++) {
 	requestControl.add(Promise.resolve(i));
 }
 
-requestControl.request().then(res => console.log(res));
+requestControl.mm().then(res => console.log(res));
